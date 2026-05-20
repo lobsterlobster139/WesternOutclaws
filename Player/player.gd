dffd9882f8657687e757680e74460c25a2103c3d
PhosphorusCat <23atcudzevich@wakatipu.school.nz>
2026-05-20 18:24:50 +1200

    Basic Movement COMPLETE!!

--- Player/player.gd
+++ Player/player.gd
@@ -1,4 +1,64 @@
-class_name PlayerController extends CharacterBody3D
+extends CharacterBody3D
 
-func update_rotation(rotation_input) -> void:
-	global_transform.basis = Basis.from_euler(rotation_input)
+@onready var head = $Head
+@onready var player = $"."
+@onready var camera = $Head/Camera3D
+
+
+const JUMP_VELOCITY = 4.5
+
+var sensitivity = 0.002
+var speed = 5.0
+
+#head_bob variables
+const BOB_FREQ = 3
+const BOB_AMP = 0.06
+var t_bob = 0.0
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		player.rotate_y(-event.relative.x * sensitivity)
+		head.rotate_x(-event.relative.y * sensitivity)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 8.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 8.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	# Head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = head_bob(t_bob)
+	
+	move_and_slide()
+
+func head_bob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * (BOB_AMP / 2)
+	return pos
